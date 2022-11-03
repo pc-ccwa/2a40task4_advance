bf129d486ef4a6dc397c8c7ae97703912d59a45b
pc-ccwa <115084257+pc-ccwa@users.noreply.github.com>
2022-11-04 07:47:33 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
-let x_value = 0
 let y_value = 0
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.clearScreen()
-        led.plot(x_value, 0)
-        x_value += 1
-        basic.pause(200)
-        if (x_value > 4) {
-            x_value = 0
-        }
-    }
-})
+let x_value = 0
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
@@ -19,6 +8,17 @@ basic.forever(function () {
         basic.pause(200)
         if (y_value > 4) {
             y_value = 0
+        }
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(x_value, 0)
+        x_value += 1
+        basic.pause(200)
+        if (x_value > 4) {
+            x_value = 0
         }
     }
 })
